7abe8e8ece1c18caaf6a74925f25208b803fa333
liangdong <1789719643@qq.com>
2022-03-25 14:58:47 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
+  <div class="home">
     <h2>Home</h2>
-    <el-button @click="getData">请求</el-button>
   </div>
 </template>
 <script>
@@ -11,11 +10,7 @@ export default {
     return {};
   },
   mounted() {},
-  methods:{
-    getData(){
-      this.$request.post('https://yapi.dooffe.com/mock/269/customer/manager/list',{name:1}).then(res=>{})
-    }
-  }
+  methods:{}
 };
 </script>
 <style lang='scss' scoped>
